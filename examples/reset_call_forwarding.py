da04d2826fed55bac53596e385dd34ed63464850
Johannes Krohn <johannes.krohn@krohns.de>
2022-11-16 11:12:40 +0100

    - print execution times in reset_call_forwarding.py

--- examples/reset_call_forwarding.py
+++ examples/reset_call_forwarding.py
@@ -5,6 +5,7 @@ Reset call forwarding to default for all users in the org
 """
 
 import logging
+import time
 from concurrent.futures import ThreadPoolExecutor
 
 from dotenv import load_dotenv
@@ -22,8 +23,11 @@ logging.getLogger('wxc_sdk.rest').setLevel(logging.INFO)
 api = WebexSimpleApi()
 
 # get all calling users
+start = time.perf_counter_ns()
 calling_users = [user for user in api.people.list(calling_data=True)
                  if user.location_id]
+print(f'Got {len(calling_users)} calling users in '
+      f'{(time.perf_counter_ns() - start) / 1e6:.3f} ms')
 
 # set call forwarding to default for all users
 with ThreadPoolExecutor() as pool:
@@ -31,7 +35,10 @@ with ThreadPoolExecutor() as pool:
     forwarding = PersonForwardingSetting.default()
 
     # schedule update for each user and wait for completion
+    start = time.perf_counter_ns()
     list(pool.map(
         lambda user: api.person_settings.forwarding.configure(person_id=user.person_id,
                                                               forwarding=forwarding),
         calling_users))
+    print(f'Reset call forwarding to default for {len(calling_users)} users in '
+          f'{(time.perf_counter_ns() - start) / 1e6:.3f} ms')
