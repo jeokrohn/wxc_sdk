c1ae8364f64c42b0dff092e9c82333917b99aece
Johannes Krohn <johannes.krohn@krohns.de>
2023-04-21 04:56:32 +0200

    - catch exceptions when scraping API docs from developer.webex.com

--- developer.webex.com/read_api_spec.py
+++ developer.webex.com/read_api_spec.py
@@ -172,5 +172,9 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except Exception:
+        print(f'failed to run {", ".join(sys.argv)}')
+        raise
     exit(0)
