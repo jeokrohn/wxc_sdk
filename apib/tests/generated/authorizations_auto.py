6a57e5de49d5829cba906c63858f67e431a53851
Johannes Krohn <johannes.krohn@krohns.de>
2024-11-22 04:46:02 +0100

    - docstrings

--- apib/tests/generated/authorizations_auto.py
+++ apib/tests/generated/authorizations_auto.py
@@ -49,7 +49,10 @@ class AuthorizationsApi(ApiChild, base='authorizations'):
     """
     Authorizations
     
-    Admin API. You need full or user level admin privileges to call this API.
+    This is an admin API. As a partner admin revoking tokens for your customer
+    users, you must be a `Full Admin.` As an admin in your own org revoking tokens
+    for your home org users, you must have either of the following roles: `Full
+    Admin,` `Device Admin,` `Or user Admin.` Read-only admins are not supported.
     
     
     
@@ -58,31 +61,31 @@ class AuthorizationsApi(ApiChild, base='authorizations'):
     authorized with specific `access scopes
     <https://developer.webex.com/docs/integrations#scopes>`_ in the oAuth client life-cycle. Integrations and some of the Webex service
     portals, like `developer.webex.com
-    <https://developer.webex.com/>`_, are all oAuth clients, each with their unique `clientId`.
+    <https://developer.webex.com/>`_, are all oAuth clients, each with their unique `clientId.`
     
     Your application receives an API `access token
     <https://developer.webex.com/docs/integrations#getting-an-access-token>`_ and a `refresh token
-    used to call Webex APIs for which the user authorized the scopes. Access tokens expire fairly frequently, while
-    refresh tokens (when being regularly used) will be refreshed to last forever (see `Using the Refresh Token
-    <https://developer.webex.com/docs/integrations#using-the-refresh-token>`_ for
-    details).
+    used to call Webex APIs for which the user authorized the scopes. Access tokens expire reasonably frequently,
+    while refresh tokens (when being regularly used) will be refreshed to last forever (see `Using the Refresh Token
+    <https://developer.webex.com/docs/integrations#using-the-refresh-token>`_
+    for details).
     
     In this API an authorization is synonymous with an `API access token
     <https://developer.webex.com/docs/integrations#getting-an-access-token>`_.
     
     To provide admins with fine-grained token management control, you use the `/authorizations
-    <https://developer.webex.com/docs/api/v1/authorizations>`_ API with
-    the `DELETE` HTTP method to revoke access and refresh tokens.
+    <https://developer.webex.com/docs/api/v1/authorizations>`_ API with the `DELETE`
+    HTTP method to revoke access and refresh tokens.
     
     Deleting a refresh token will revoke all associated access tokens as well. Deleting an access token will revoke the
-    developers ability to call the APIs with it.
-    Webex subsystems may cache the validity of the token for a short while longer after the authorization was deleted.
+    developer's ability to call the APIs with it.
+    Webex subsystems may cache the token's validity briefly after the authorization is deleted.
     
     Admins can revoke user authorizations for users in their organization. When an admin deletes their own token, the
-    `clientId` used to auhtorize the request must match the `clientId` used to generate the token.
+    `clientId` used to authorize the request must match the `clientId` used to generate the token.
     
-    To use the authorizations API in an Integration the scopes must include:
-    `identity:tokens_write`,`identity:tokens_read`.
+    To use the authorizations API in an Integration the scopes must include: `identity:tokens_write,`
+    `identity:tokens_read.`
     """
 
     def list_authorizations_for_a_user(self, person_id: str, person_email: str) -> list[Authorization]:
@@ -110,9 +113,9 @@ class AuthorizationsApi(ApiChild, base='authorizations'):
         """
         Delete authorization
 
-        Deletes an authorization, by authorization ID.
+        Deletes an authorization by authorization ID.
 
-        Specify the authorization Id in the `authorizationId` parameter in the URI which was listed in the list
+        Specify the authorization Id in the `authorizationId` parameter in the URI, which was listed in the list
         resource.
 
         :param authorization_id: The unique identifier for the message.
@@ -126,7 +129,7 @@ class AuthorizationsApi(ApiChild, base='authorizations'):
         """
         Delete authorization of org and client ID
 
-        Deletes an authorization, by org ID and client ID.
+        Deletes an authorization by org ID and client ID.
 
         :param client_id: The unique oAuth client id.
         :type client_id: str
