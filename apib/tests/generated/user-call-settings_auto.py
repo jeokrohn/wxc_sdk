1d939002178eab0bb8d33bd3fab7dd38e460e4cb
Johannes Krohn <johannes.krohn@krohns.de>
2025-03-13 12:29:43 +0100

    - already covered

--- apib/tests/generated/user-call-settings_auto.py
+++ apib/tests/generated/user-call-settings_auto.py
@@ -27,7 +27,7 @@ __all__ = ['Action', 'AgentCallerIdType', 'ApplicationsSetting', 'AudioAnnouncem
            'CallerIdInfoExternalCallerIdNamePolicy', 'CallerIdInfoSelected', 'CountObject', 'DeviceType',
            'DoNotDisturbInfo', 'EndpointInformation', 'Endpoints', 'ErrorMessageObject', 'ErrorObject',
            'EventLongDetails', 'EventLongDetailsRecurrence', 'EventLongDetailsRecurrenceRecurDaily',
-           'EventLongDetailsRecurrenceRecurWeekly', 'GetCallingBehaviorObject',
+           'EventLongDetailsRecurrenceRecurWeekly', 'ExceptionTypeObject', 'GetCallingBehaviorObject',
            'GetCallingBehaviorObjectBehaviorType', 'GetCallingBehaviorObjectEffectiveBehaviorType', 'GetEvent',
            'GetMessageSummaryResponse', 'GetMonitoredElementsObject', 'GetMonitoredElementsObjectCallparkextension',
            'GetMonitoredElementsObjectMember', 'GetMusicOnHoldObject', 'GetNumbers', 'GetNumbersPhoneNumbers',
@@ -35,9 +35,10 @@ __all__ = ['Action', 'AgentCallerIdType', 'ApplicationsSetting', 'AudioAnnouncem
            'GetSharedLineMemberItem', 'GetSharedLineMemberList', 'GetUserMSTeamsSettingsObject',
            'IncomingPermissionSetting', 'IncomingPermissionSettingExternalTransfer', 'ItemObject',
            'JobDetailsResponse', 'JobDetailsResponseById', 'JobDetailsResponseLatestExecutionExitCode',
-           'JobExecutionStatusObject', 'LineType', 'Location', 'ModifyUserMSTeamsSettingsObjectSettingName',
-           'MonitoredMemberObject', 'MonitoredNumberObject', 'MonitoringSettings', 'NumberOwnerType',
-           'OutgoingCallingPermissionsSettingGet', 'OutgoingCallingPermissionsSettingGetCallingPermissions',
+           'JobExecutionStatusObject', 'LineType', 'Location', 'ModeManagementFeatureTypeObject',
+           'ModifyUserMSTeamsSettingsObjectSettingName', 'MonitoredMemberObject', 'MonitoredNumberObject',
+           'MonitoringSettings', 'NumberOwnerType', 'OutgoingCallingPermissionsSettingGet',
+           'OutgoingCallingPermissionsSettingGetCallingPermissions',
            'OutgoingCallingPermissionsSettingGetCallingPermissionsAction',
            'OutgoingCallingPermissionsSettingGetCallingPermissionsCallType',
            'OutgoingCallingPermissionsSettingPutCallingPermissions', 'PeopleOrPlaceOrVirtualLineType',
@@ -52,6 +53,7 @@ __all__ = ['Action', 'AgentCallerIdType', 'ApplicationsSetting', 'AudioAnnouncem
            'SettingsObjectSettingName', 'StartJobExecutionStatusObject', 'StartJobExecutionStatusObjectExitCode',
            'StartJobResponseObject', 'StartJobResponseObjectLatestExecutionStatus', 'StepExecutionStatusesObject',
            'TelephonyType', 'TransferNumberGet', 'UserCallSettingsApi', 'UserDigitPatternObject', 'UserItem',
+           'UserModeManagementAvailableFeaturesObject', 'UserModeManagementFeatureObject',
            'UserOutgoingPermissionDigitPatternGetListObject', 'UserPlaceAuthorizationCodeListGet', 'UsersListItem',
            'VoiceMailPartyInformation', 'VoiceMessageDetails', 'VoicemailInfo', 'VoicemailInfoEmailCopyOfMessage',
            'VoicemailInfoFaxMessage', 'VoicemailInfoMessageStorage', 'VoicemailInfoMessageStorageStorageType',
@@ -1994,6 +1996,82 @@ class PersonalAssistantGet(ApiModel):
     alert_me_first_number_of_rings: Optional[int] = None
 
 
+class ModeManagementFeatureTypeObject(str, Enum):
+    #: Specifies the feature is an Auto Attendant.
+    auto_attendant = 'AUTO_ATTENDANT'
+    #: Specifies the feature is a Call Queue.
+    call_queue = 'CALL_QUEUE'
+    #: Specifies the feature is a Hunt Group.
+    hunt_group = 'HUNT_GROUP'
+
+
+class UserModeManagementAvailableFeaturesObject(ApiModel):
+    #: A unique identifier for the feature.
+    #: example: Y2lzY29zcGFyazovL3VzL0FVVE9fQVRURU5EQU5ULzA1NTJmNjdiLTU5YTktNDFiYi04NzM2LTFiMDQxZDFkZGQ1ZQ
+    id: Optional[str] = None
+    #: Unique name for the feature.
+    #: example: Test Feature
+    name: Optional[str] = None
+    #: Defines the scheduling of the operating mode.
+    #: example: AUTO_ATTENDANT
+    type: Optional[ModeManagementFeatureTypeObject] = None
+    #: The primary phone number configured for the feature.
+    #: example: +19705550028
+    phone_number: Optional[str] = None
+    #: The extension configured for the feature.
+    #: example: 28
+    extension: Optional[str] = None
+
+
+class ExceptionTypeObject(str, Enum):
+    #: The mode was switched to or extended by the user for manual switch back and runs as an exception until the user
+    #: manually switches the feature back to normal operation or a different mode.
+    manual_switch_back = 'MANUAL_SWITCH_BACK'
+    #: The mode was switched to by the user before its start time and runs as an exception until its end time is
+    #: reached, at which point it automatically switches the feature back to normal operation.
+    automatic_switch_back_early_start = 'AUTOMATIC_SWITCH_BACK_EARLY_START'
+    #: The current mode was extended by the user before its end time and runs as an exception until the extension end
+    #: time (mode's end time + extension of up to 12 hours) is reached, at which point it automatically switches the
+    #: feature back to normal operation.
+    automatic_switch_back_extension = 'AUTOMATIC_SWITCH_BACK_EXTENSION'
+    #: The mode will remain the current operating mode for the feature until its normal end time is reached.
+    automatic_switch_back_standard = 'AUTOMATIC_SWITCH_BACK_STANDARD'
+
+
+class UserModeManagementFeatureObject(ApiModel):
+    #: A unique identifier for the feature.
+    #: example: Y2lzY29zcGFyazovL3VzL0FVVE9fQVRURU5EQU5ULzA1NTJmNjdiLTU5YTktNDFiYi04NzM2LTFiMDQxZDFkZGQ1ZQ
+    id: Optional[str] = None
+    #: Unique name for the feature.
+    #: example: Test Feature
+    name: Optional[str] = None
+    #: Defines the scheduling of the operating mode.
+    type: Optional[ModeManagementFeatureTypeObject] = None
+    #: The primary phone number configured for the feature.
+    #: example: +19705550028
+    phone_number: Optional[str] = None
+    #: The extension configured for the feature.
+    #: example: 28
+    extension: Optional[str] = None
+    #: A flag to indicate whether mode-based call forwarding is enabled for the feature.
+    #: example: True
+    mode_based_forwarding_enabled: Optional[bool] = None
+    #: The destination for call forwarding if mode-based call forwarding is enabled.
+    #: example: +19705550028
+    forward_destination: Optional[str] = None
+    #: Name of the current operating mode.
+    #: example: Day Operating Mode
+    current_operating_mode_name: Optional[str] = None
+    #: Unique identifier for the current operating mode.
+    #: example: Y2lzY29zcGFyazovL3VzL09QRVJBVElOR19NT0RFLzIxM2I1ZTIwLTljMjAtNDUwNi1iYWE3LTE0OGE0NWQyZWY4OQ
+    current_operating_mode_id: Optional[str] = None
+    #: Defines the exception through which the current operating mode is set as active for the feature.
+    #: example: MANUAL_SWITCH_BACK
+    exception_type: Optional[ExceptionTypeObject] = None
+    #: Location object that has a unique identifier for the location and its name.
+    location: Optional[Location] = None
+
+
 class RetrieveExecutiveAssistantSettingsForAPersonResponseType(str, Enum):
     #: The feature is not enabled.
     unassigned = 'UNASSIGNED'
@@ -2399,7 +2477,7 @@ class UserCallSettingsApi(ApiChild, base=''):
         platform for replay and archival. This feature is helpful for quality assurance, security, training, and more.
 
         This API requires a full or user administrator or location administrator auth token with the
-        `spark-admin:people_write` scope.
+        `spark-admin:people_read` scope.
 
         <div><Callout type="warning">A person with a Webex Calling Standard license is eligible for the Call Recording
         feature only when the Call Recording vendor is Webex.</Callout></div>
@@ -5735,4 +5813,103 @@ class UserCallSettingsApi(ApiChild, base=''):
         if alert_me_first_number_of_rings is not None:
             body['alertMeFirstNumberOfRings'] = alert_me_first_number_of_rings
         url = self.ep(f'telephony/config/people/{person_id}/features/personalAssistant')
+        super().put(url, params=params, json=body)
+
+    def retrieve_the_list_of_available_features_(self, person_id: str = None, name: str = None,
+                                                 phone_number: str = None, extension: str = None, order: str = None,
+                                                 org_id: str = None,
+                                                 **params) -> Generator[UserModeManagementAvailableFeaturesObject, None, None]:
+        """
+        Retrieve the List of Available Features.
+
+        Retrieve a list of feature identifiers that can be assigned to a user for `Mode Management`. Feature
+        identifiers reference feature instances like `Auto Attendants`, `Call Queues`, and `Hunt Groups`.
+
+        Features with mode-based call forwarding enabled can be assigned to a user for `Mode Management`.
+
+        Retrieving this list requires a full, read-only, or location administrator auth token with a scope of
+        `spark-admin:telephony_config_read`.
+
+        :param person_id: A unique identifier for the user.
+        :type person_id: str
+        :param name: List features whose `name` contains this string.
+        :type name: str
+        :param phone_number: List features whose phoneNumber contains this matching string.
+        :type phone_number: str
+        :param extension: List features whose `extension` contains this matching string.
+        :type extension: str
+        :param order: Sort the list of features based on `name`, `phoneNumber`, or `extension`, either `asc`, or
+            `desc`.
+        :type order: str
+        :param org_id: Retrieve features list from this organization.
+        :type org_id: str
+        :return: Generator yielding :class:`UserModeManagementAvailableFeaturesObject` instances
+        """
+        if name is not None:
+            params['name'] = name
+        if phone_number is not None:
+            params['phoneNumber'] = phone_number
+        if extension is not None:
+            params['extension'] = extension
+        if order is not None:
+            params['order'] = order
+        if org_id is not None:
+            params['orgId'] = org_id
+        url = self.ep(f'telephony/config/people/{person_id}/modeManagement/availableFeatures')
+        return self.session.follow_pagination(url=url, model=UserModeManagementAvailableFeaturesObject, item_key='features', params=params)
+
+    def retrieve_the_list_of_features_assigned_to_a_user_for_mode_management_(self, person_id: str = None,
+                                                                              org_id: str = None) -> list[UserModeManagementFeatureObject]:
+        """
+        Retrieve the List of Features Assigned to a User for Mode Management.
+
+        Retrieve a list of feature identifiers that are already assigned to a user for `Mode Management`. Feature
+        identifiers reference feature instances like `Auto Attendants`, `Call Queues`, and `Hunt Groups`.
+        A maximum of 50 features can be assigned to a user for `Mode Management`.
+
+        Features with mode-based call forwarding enabled can be assigned to a user for `Mode Management`.
+
+        Retrieving this list requires a full, read-only, or location administrator auth token with a scope of
+        `spark-admin:telephony_config_read`.
+
+        :param person_id: A unique identifier for the user.
+        :type person_id: str
+        :param org_id: Retrieve features list from this organization.
+        :type org_id: str
+        :rtype: list[UserModeManagementFeatureObject]
+        """
+        params = {}
+        if org_id is not None:
+            params['orgId'] = org_id
+        url = self.ep(f'telephony/config/people/{person_id}/modeManagement/features')
+        data = super().get(url, params=params)
+        r = TypeAdapter(list[UserModeManagementFeatureObject]).validate_python(data['features'])
+        return r
+
+    def assign_a_list_of_features_to_a_user_for_mode_management_(self, feature_ids: list[str], person_id: str = None,
+                                                                 org_id: str = None):
+        """
+        Assign a List of Features to a User for Mode Management.
+
+        Assign a user a list of feature identifiers for `Mode Management`. Feature identifiers reference feature
+        instances like `Auto Attendants`, `Call Queues`, and `Hunt Groups`.
+        A maximum of 50 features can be assigned to a user for `Mode Management`.
+
+        Updating mode management settings for a user requires a full, or location administrator auth token with a scope
+        of `spark-admin:telephony_config_write`.
+
+        :param feature_ids: Array of feature IDs.
+        :type feature_ids: list[str]
+        :param person_id: A unique identifier for the user.
+        :type person_id: str
+        :param org_id: Retrieve features list from this organization.
+        :type org_id: str
+        :rtype: None
+        """
+        params = {}
+        if org_id is not None:
+            params['orgId'] = org_id
+        body = dict()
+        body['featureIds'] = feature_ids
+        url = self.ep(f'telephony/config/people/{person_id}/modeManagement/features')
         super().put(url, params=params, json=body)
